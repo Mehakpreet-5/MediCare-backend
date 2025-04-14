efb4c74bce885a3d6fd04218239c19f5d5372da1
Mehakpreet-5 <themehakk@gmail.com>
2025-04-14 20:25:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.get("/data", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+// process.env.PORT ||
 // app.listen(5000, () => console.log("Server running on port 5000"));
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
